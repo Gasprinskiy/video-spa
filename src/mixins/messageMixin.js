25fb44ed346679e62058ab0d01c4310e1cb20ca1
azabroflovski <azabroflovski@gmail.com>
2022-04-10 20:18:02 +0500

    feat(mixins): add request mixin

--- src/mixins/messageMixin.js
+++ src/mixins/messageMixin.js
@@ -1,40 +1,59 @@
+import { apiRequest } from '../apiWorkers/apiRequest'
 
 export default {
-    data(){
+    data() {
         return {
-            message: '',
-            errorMessege: '',
-            isError: false,
-            isWarn: false,
-            isSuccess: false,
+            request: {
+                loading: false,
+                initiator: null,
+                autoErrorHandling: false,
+                status: 'pending'
+            }
         }
     },
 
-    watch: {
-        message(){
-            if(this.message !== ''){
-               this.$q.notify({
-                    message: this.message,
-                    color: this.messageColor
-                })
-            }
-            this.message = ''
+    methods: {
+        prepareRequest() {
+            const { httpQuery, autoErrorHandling } = this.requestBuilder()
+            this.request.autoErrorHandling = autoErrorHandling
+            this.request.initiator = httpQuery.initiator
+            this.request.options = httpQuery.options
         },
 
-    },
+        async makeRequest() {
+            try {
+                this.request.status = 'loading'
+                this.request.loading = true
+                this.prepareRequest()
 
-    computed: {
-        messageColor(){
-            if(this.isError){
-                this.isError = false
-                return 'red'
-            }else if(this.isWarn){
-                this.isWarn = false
-                return 'orange'
-            } else if(this.isSuccess){
-                this.isSuccess = false
-                return 'green'
+                const response = await apiRequest(this.request.initiator, this.request.options)
+                this.requestSuccess(response)
+            } catch (error) {
+                this.request.status = 'error'
+                this.requestFailed(error)
+
+                if (this.request.autoErrorHandling) {
+                    this.$q.notify({
+                        message: error,
+                        color: 'negative'
+                    })
+                }
+
+            } finally {
+                this.request.loading = false
             }
+        },
+
+        requestBuilder() {
+            throw 'requestBuilder not defined'
+        },
+
+        requestSuccess() {
+            throw 'successRequest not defined'
+        },
+
+        requestFailed() {
+            throw 'failedRequest request not defined'
         }
-    }
+    },
 }
