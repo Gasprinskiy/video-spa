6d51e372f65647e421aa7dcedfe9a8ab1546e134
Gasprinskiy <85307942+Gasprinskiy@users.noreply.github.com>
2022-04-10 18:39:05 +0500

    player iframe

---
+++ src/components/playerItems/iFramePlayer.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="video-iframe-player">
+        <iframe class="ifarame"  frameborder="0" allowfullscreen  
+            :src="url">
+        </iframe>
+    </div>
+</template>
+
+<script>
+
+//https://www.youtube.com/embed/${videoId}?autoplay=1
+
+export default {
+    props: {
+        url: {
+            type: String,
+            default: '',
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .video-iframe-player {
+        width: 100%;
+        max-height: 520px;
+        padding-bottom: 50%;
+        // flex-grow: 1;
+        position: relative;
+       .ifarame {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+       } 
+    }
+</style>
