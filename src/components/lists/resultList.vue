25fc57951f7476c8b3225aedf21785d670401172
Gasprinskiy <85307942+Gasprinskiy@users.noreply.github.com>
2022-04-10 18:37:09 +0500

    video result list, with top-title and v-for cycle

--- src/components/lists/resultList.vue
+++ src/components/lists/resultList.vue
@@ -1,23 +1,32 @@
 <template>
+    <div class="result-list-wrapper" :class="{small: small}">
+        <top-title class="result-list-title" :title="title"/>
         <div class="result-list" :class="{small: small}">
             <div 
                 class="result-list-item"
-
                 v-for="result in resultList"
                 :key="result.id"
-                @click="$router.push(`/video/${result.id}`)"
+                @click="$emit('result-methods', result.videoId)"
                 >
+                <q-btn 
+                    flat
+                    class="result-list-item-route-btn" 
+                    @click="$router.push(`/video/${result.videoId}`)"
+                />
                 <result-item :small="small" :result="result"/>
             </div>
         </div>
+    </div>
+        
 </template>
 
 <script>
 
 import resultItem from '@/components/items/resultItem.vue'
+import topTitle from '@/components/templates/topTitle.vue'
 
 export default {
-    components: {resultItem},
+    components: {resultItem, topTitle},
     props: {
         resultList: {
             type: Array,
@@ -26,6 +35,10 @@ export default {
         small: {
             type: Boolean,
             default: false
+        },
+        title: {
+            type: String,
+            default: ''
         }
     },
     methods: {
@@ -37,20 +50,43 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    .result-list-wrapper {
+        padding: 20px 10px 0px 10px;
+        .result-list-title {
+            margin-bottom: 10px;
+        }
+        &.small {
+            padding: 0;
+            .result-list-title {
+                margin-bottom: 0px;
+            }
+        }
+    }
     .result-list {
         width: 100%;
-        padding: 20px 10px 0px 10px;
         .result-list-item {
-            cursor: pointer;
+            position: relative;
+            display: flex;
+            align-items: center;
             .result-item {
                 pointer-events: none;
+            }
+            .result-slot {
+                position: absolute;
+                right: 5px;
+                z-index: 101;
+            }
+            .result-list-item-route-btn {
+                position: absolute;
+                width: 100%;
+                height: 100%;
+                z-index: 100;
             }
         }
         .result-list-item:not(:last-child){
             margin-bottom: 20px;
         }
         &.small {
-            padding: 0;
            .result-list-item:not(:last-child){
                 margin-bottom: 10px;
             }
