5a0b68cdd9a49bb1d798f18aef13099239ef4dc7
Gasprinskiy <85307942+Gasprinskiy@users.noreply.github.com>
2022-04-10 18:45:40 +0500

    title template

---
+++ src/components/templates/topTitle.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="top-title-block">
+        <div class="text-h6" v-html="title"/>
+        <div class="top-title-slot">
+            <slot name="top-title-slot"/>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            default: ''
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .top-title-block {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
