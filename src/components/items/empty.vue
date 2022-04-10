6ca53272d6a945b1917709e2620f7ba6f5927a2c
Gasprinskiy <85307942+Gasprinskiy@users.noreply.github.com>
2022-04-10 18:35:03 +0500

    component empty message

---
+++ src/components/items/empty.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="empty-wrapper">
+        <div class="empty">
+            <q-icon v-if="message !== ''" class="empty-icon" name="folder_off" size="40px" color="grey"/>
+            <div class="empty-message text-subtitle">{{message}}</div>
+        </div> 
+    </div>
+        
+</template>
+
+<script>
+export default {
+    props: {
+        message: {
+            type: String,
+            default: ''
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .empty-wrapper {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 20px;
+    }
+    .empty {
+        width: 100%;
+        text-align: center;
+        max-width: 350px;
+        .empty-icon {
+            margin-bottom: 15px;
+        }
+        .empty-message {
+            font-size: 17px;
+            color: grey;
+        }
+    }
+</style>
